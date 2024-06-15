5398e213a308763638e236db6315ffdcbb1c08a2
KirbyStore <oeoe59383@gmail.com>
2024-06-15 05:14:24 +0200

    Update verify-1-0.js

--- src/verify-1-0.js
+++ src/verify-1-0.js
@@ -111,7 +111,7 @@ window.onload = async (event) => {
 			var verifySuccessDesc = serverDescElement.getAttribute("vc-verify-success");
 			
 			serverTitleElement.textContent = verifySuccessTitle ?? "Verified!";
-			serverDescElement.innerHTML = verifySuccessDesc !== 'custom' ? verifySuccessDesc : "Complete! Service provided by <a class=\"text-red-500 animate-pulse\" href=\"https://vaultcord.com\">vaultcord.com</a> | <a class=\"text-red-500 animate-pulse\" href=\"https://vaultcord.com/privacy\">Privacy Policy</a>";
+			serverDescElement.innerHTML = verifySuccessDesc !== 'custom' ? verifySuccessDesc : "You have Complete the Verify Thanks you! You can return to Kirby Store.";
 		}
 		else {
 			document.getElementById("failureCross").hidden=false;
